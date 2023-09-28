abbc58a26344dda46bf6f1692c3dc22825b3f853
mark <markpackham1@gmail.com>
2023-09-28 11:17:17 +0100

    add bob profile

--- L2T03 - React - Elements/Compulsory Task 2/ReactElements/react-hello/src/App.jsx
+++ L2T03 - React - Elements/Compulsory Task 2/ReactElements/react-hello/src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 
+const user = {
+  name: "Bob",
+  surname: "Smith",
+  date_of_birth: "01/01/1990",
+  address: "123 Street Road",
+  country: "Wales",
+  email: "bob@gmail.com",
+  telephone: 123456789,
+  company: "Microsoft",
+  profile_picture:
+    "https://techcrunch.com/wp-content/uploads/2019/07/Bob-Smith_portrait-1.jpg?w=490&crop=2",
+};
+
 function App() {
   const [count, setCount] = useState(0);
 
