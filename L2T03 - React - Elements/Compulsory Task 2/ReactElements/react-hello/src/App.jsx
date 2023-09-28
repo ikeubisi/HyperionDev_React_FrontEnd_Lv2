5ba87197640633aeaa233a7d19c819dddf8b8557
mark <markpackham1@gmail.com>
2023-09-28 12:00:24 +0100

    output user

--- L2T03 - React - Elements/Compulsory Task 2/ReactElements/react-hello/src/App.jsx
+++ L2T03 - React - Elements/Compulsory Task 2/ReactElements/react-hello/src/App.jsx
@@ -5,7 +5,7 @@ const user = {
   name: "Bob",
   surname: "Smith",
   date_of_birth: "01/01/1990",
-  address: "123 Street Road",
+  address: "123 Street, Some Town 777HHHRRR",
   country: "Wales",
   email: "bob@gmail.com",
   telephone: 123456789,
@@ -15,13 +15,31 @@ const user = {
   shopping_cart: ["shoes", "socks", "tie", "coat"],
 };
 
-const shopping_cart = ["shoes", "socks", "tie", "coat"];
+const username = user.name + " " + user.surname;
 
 const shoppingList = (
   <div>
-    {shopping_cart.map((item, index) => {
-      return <li key={index}>{item}</li>;
-    })}
+    <h1>{username}</h1>
+    <h4>Born: {user.date_of_birth}</h4>
+    <ul>
+      <li>{user.address}</li>
+      <li>{user.country}</li>
+      <li>{user.email}</li>
+      <li>{user.telephone}</li>
+      <li>Company: {user.company}</li>
+    </ul>
+
+    <img src={user.profile_picture} alt={username} />
+
+    <h2>Full user object output</h2>
+    <ul>
+      {/* Object entries learned from MDN Webdocs
+      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries */}
+      {/* key must be unique */}
+      {Object.entries(user).map(([key, value]) => {
+        return <li key={key}>{value}</li>;
+      })}
+    </ul>
   </div>
 );
 
