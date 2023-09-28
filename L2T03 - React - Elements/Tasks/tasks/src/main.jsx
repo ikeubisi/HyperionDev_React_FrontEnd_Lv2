29820955742371e92cdcc9023d056f6fa29d0a12
mark <markpackham1@gmail.com>
2023-09-28 08:36:54 +0100

    demo image output

--- L2T03 - React - Elements/Tasks/tasks/src/main.jsx
+++ L2T03 - React - Elements/Tasks/tasks/src/main.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const name = "Hyper Dave";
-const element = <h1>Welcome back, {name}</h1>;
+
+// const name = "Hyper Dave";
+// const element = <h1>Welcome back, {name}</h1>;
+
+const element = (
+  <div>
+    <h1>I'm learning React with HyperionDev</h1>
+    <img
+      src="https://www.hyperiondev.com/static/images/logo.png"
+      style={{ background: "grey" }}
+    />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>{element}</React.StrictMode>);
