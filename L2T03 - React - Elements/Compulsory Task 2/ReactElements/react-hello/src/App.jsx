c0fec7b12fe1163da4b275726827e0db905b50a0
mark <markpackham1@gmail.com>
2023-09-28 11:42:21 +0100

    output shopping list

--- L2T03 - React - Elements/Compulsory Task 2/ReactElements/react-hello/src/App.jsx
+++ L2T03 - React - Elements/Compulsory Task 2/ReactElements/react-hello/src/App.jsx
@@ -12,12 +12,23 @@ const user = {
   company: "Microsoft",
   profile_picture:
     "https://techcrunch.com/wp-content/uploads/2019/07/Bob-Smith_portrait-1.jpg?w=490&crop=2",
+  shopping_cart: ["shoes", "socks", "tie", "coat"],
 };
+
+const shopping_cart = ["shoes", "socks", "tie", "coat"];
+
+const shoppingList = (
+  <div>
+    {shopping_cart.map((item, index) => {
+      return <li key={index}>{item}</li>;
+    })}
+  </div>
+);
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Hello</>;
+  return <>{shoppingList}</>;
 }
 
 export default App;
