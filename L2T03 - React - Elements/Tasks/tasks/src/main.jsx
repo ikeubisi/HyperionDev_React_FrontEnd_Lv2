605845cabe5d7a6eb41271cc373d74c5175fa348
mark <markpackham1@gmail.com>
2023-09-28 09:43:19 +0100

    demo ternary use

--- L2T03 - React - Elements/Tasks/tasks/src/main.jsx
+++ L2T03 - React - Elements/Tasks/tasks/src/main.jsx
@@ -4,14 +4,20 @@ import ReactDOM from "react-dom/client";
 // const name = "Hyper Dave";
 // const element = <h1>Welcome back, {name}</h1>;
 
+// const element = (
+//   <div>
+//     <h1>I'm learning React with HyperionDev</h1>
+//     <img
+//       src="https://www.hyperiondev.com/static/images/logo.png"
+//       style={{ background: "grey" }}
+//     />
+//   </div>
+// );
+
+// demo use of ternary
+const person = "Dave";
 const element = (
-  <div>
-    <h1>I'm learning React with HyperionDev</h1>
-    <img
-      src="https://www.hyperiondev.com/static/images/logo.png"
-      style={{ background: "grey" }}
-    />
-  </div>
+  <div>{person === "Dave" ? <p>Hi, Dave!</p> : <p>Hi, John!</p>}</div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
