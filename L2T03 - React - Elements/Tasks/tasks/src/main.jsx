fbc1112382d9968645f915f497ee5fee570bf2e4
mark <markpackham1@gmail.com>
2023-09-28 10:32:45 +0100

    demo element tasks

--- L2T03 - React - Elements/Tasks/tasks/src/main.jsx
+++ L2T03 - React - Elements/Tasks/tasks/src/main.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 // const name = "Hyper Dave";
 // const element = <h1>Welcome back, {name}</h1>;
 
@@ -21,13 +18,35 @@ import ReactDOM from "react-dom/client";
 // );
 
 // demo use of Array map
-const itemList = ["Apples", "Strawberries", "Bananas", "Nectarines"];
+// const itemList = ["Apples", "Strawberries", "Bananas", "Nectarines"];
+// const element = (
+//   <div>
+//     {itemList.map((item, index) => {
+//       return <li key={index}>{item}</li>;
+//     })}
+//   </div>
+// );
+
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+const currentDateTime = new Date().toLocaleString();
+
 const element = (
-  <div>
-    {itemList.map((item, index) => {
-      return <li key={index}>{item}</li>;
-    })}
-  </div>
+  <>
+    <h1>Current Date Time {currentDateTime}</h1>
+
+    <a href="https://github.com/markpackham" target="_blank">
+      A Link to my Github
+    </a>
+
+    <h2>React advantages over Vanilla JS</h2>
+    <ul>
+      <li>Consistent Standards for a frontend layout</li>
+      <li>Better performance - far less page changes due to the Virtual DOM</li>
+      <li>Supported by the giant company Meta</li>
+    </ul>
+  </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
