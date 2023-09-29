791c183a68716a40c64151b3ac2994fd9598128d
mark <markpackham1@gmail.com>
2023-09-29 10:01:34 +0100

    use bootstrap components in App.js

--- L2T04 - React - Components/Tasks/reactexample/react_functional_components/src/App.js
+++ L2T04 - React - Components/Tasks/reactexample/react_functional_components/src/App.js
@@ -1,19 +1,28 @@
-// Import CSS files for styling purposes
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// importing the Welcome Component
 import Welcome from "./components/Welcome";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 function App() {
   return (
-    <div>
-      {/* Display an instance of the Welcome component
-passing a prop name="Joe Soap" */}
-      <Welcome name="Joe Soap" age="39" />{" "}
-      {/* Display a second instance of the Welcome component
-passing a prop name="John Smith" */}
-      <Welcome name="John Smith" age="52" />{" "}
+    <div className="App">
+      <hr />
+      <Welcome name="Joe Soap" age="23" />
+      <Welcome name="John Smith" age="39" />
+      <hr />
+      {/* Adding standard CSS styling to an HTML component in React */}
       <h1 style={{ backgroundColor: "lightblue" }}>Hello Style!</h1>
+      {/* A Standard HTML Button Component */}
+      <button> a Regular HTML Button</button>
+      <hr />
+      {/* A Bootstrap Button Component */}
+      <Button>a Standard Bootstrap Button</Button>
+      <hr />
+      {/* These are variants of the same Bootstrap button */}
+      <Button variant="outline-secondary">a Bootstrap Outline Button</Button>
+      <hr />
+      <Button variant="success">a Bootstrap Success Button</Button>
+      <hr />
+      <Button variant="link">Link</Button>
     </div>
   );
 }
