c8a34a268936146121e0414e3d385f477e46361b
mark <markpackham1@gmail.com>
2023-10-05 16:29:05 +0100

    show use of currency formatter

--- L2T05 - React - Local State Management and Events/Compulsory Task/ReactComponents/react-interest-calculator/src/components/DisplayMoney.jsx
+++ L2T05 - React - Local State Management and Events/Compulsory Task/ReactComponents/react-interest-calculator/src/components/DisplayMoney.jsx
@@ -8,13 +8,22 @@ import ChangeBalance from "./balanceChangers/ChangeBalance";
 // Parent component
 export default function DisplayMoney() {
   // Balance is shared by child components
-  let [balance, setBalance] = useState(100);
+  let [balance, setBalance] = useState(10000);
 
   let deposit = 0;
   let withdrawal = 0;
   // Use fixed values for bank fees and interest rates
   const interest = 0.05;
   const bankFees = 10;
+
+  // Use of Intl.NumberFormat learned from
+  // How to format numbers as currency strings (1955) Stack Overflow.
+  // Available at: https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-strings
+  // (Accessed: 05 October 2023).
+  const currencyFormatter = new Intl.NumberFormat("en-UK", {
+    style: "currency",
+    currency: "GBP",
+  });
 
   // Deposit money
   function addMoney() {
@@ -62,7 +71,7 @@ export default function DisplayMoney() {
 
   return (
     <div>
-      <h2>Current Balance £{parseFloat(balance.toFixed(2))}</h2>
+      <h2>Current Balance {currencyFormatter.format(balance)}</h2>
       <hr />
       <DepositMoney balance={balance} addMoney={addMoney} />
       <hr />
