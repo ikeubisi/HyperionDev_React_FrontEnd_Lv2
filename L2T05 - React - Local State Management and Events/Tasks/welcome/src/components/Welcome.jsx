0dbdc6eb3621e6fa186c5eb894a602fe37fab1fd
mark <markpackham1@gmail.com>
2023-10-05 08:07:11 +0100

    destructure props

--- L2T05 - React - Local State Management and Events/Tasks/welcome/src/components/Welcome.jsx
+++ L2T05 - React - Local State Management and Events/Tasks/welcome/src/components/Welcome.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 
-function Welcome(props) {
+function Welcome({ name, age }) {
   // An event handler function that displays a message in the console
   const displayAge = () => {
-    console.log(props.age);
+    console.log(age);
   };
 
   // An event handler function that changes the background color of the web page
@@ -17,7 +17,7 @@ function Welcome(props) {
   return (
     <div>
       {/* Display the name value of the props object */}
-      <h1>Hello World, {props.name}</h1>
+      <h1>Hello World, {name}</h1>
       {/* This button will call the displayAge function when clicked */}
       <button onClick={displayAge}>Display The User Age</button>
       {/* This button will call the changeBackgroundColor function when
