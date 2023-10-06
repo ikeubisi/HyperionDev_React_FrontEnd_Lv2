cc2bc6630e030200f08d213a33da75249ac81c67
mark <markpackham1@gmail.com>
2023-10-06 08:56:24 +0100

    describe the autofocus on mount field

--- L2T06 - React - Hooks/Tasks/useRefAutoFocusInput/src/components/AutoFocusInput.jsx
+++ L2T06 - React - Hooks/Tasks/useRefAutoFocusInput/src/components/AutoFocusInput.jsx
@@ -11,11 +11,15 @@ export default function AutoFocusInput() {
 
   return (
     <div>
+      <h3>This input field is set to autofocus on mount</h3>
       <input
         ref={inputRef}
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+      <br />
+      <h3>This input field is not set to autofocus on mount</h3>
+      <input />
     </div>
   );
 }
