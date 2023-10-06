72464992866b7f4ab396b50f207f7a2218740450
mark <markpackham1@gmail.com>
2023-10-06 14:35:04 +0100

    add more weather data

--- L2T06 - React - Hooks/Compulsory Task 2/city_weather/src/components/FetchWeather.jsx
+++ L2T06 - React - Hooks/Compulsory Task 2/city_weather/src/components/FetchWeather.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 export default function FetchWeather({ city }) {
+  FetchWeather.propTypes = {
+    city: PropTypes.string.isRequired,
+  };
+
   // Learned to import API Key in Vite from
   // How to create env variables in react (2023) YouTube.
   // Available at: https://www.youtube.com/shorts/r92aHr752Bg (Accessed: 06 October 2023).
@@ -8,6 +13,9 @@ export default function FetchWeather({ city }) {
 
   let siteUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
 
+  const [region, setRegion] = useState("");
+  const [text, setText] = useState("");
+  const [precip_mm, setPrecip] = useState(0);
   const [cloud, setCloud] = useState(0);
   const [temp_c, setTemp_c] = useState(0);
   const [wind_kph, setWind_kph] = useState(0);
@@ -18,10 +26,16 @@ export default function FetchWeather({ city }) {
       try {
         const response = await fetch(siteUrl);
         const data = await response.json();
-        console.log(data);
+        const region = data.location.region;
+        const text = data.current.condition.text;
+        const precip_mm = data.current.precip_mm;
         const cloud = data.current.cloud;
         const temp_c = data.current.temp_c;
         const wind_kph = data.current.wind_kph;
+
+        setRegion(region);
+        setText(text);
+        setPrecip(precip_mm);
         setCloud(cloud);
         setTemp_c(temp_c);
         setWind_kph(wind_kph);
@@ -37,5 +51,16 @@ export default function FetchWeather({ city }) {
     // Don't do anything unless we have a siteUrl and a city
   }, [siteUrl, city]);
 
-  return `Temperature is ${temp_c} Celsius, Cloud is at ${cloud}, Wind is at ${wind_kph}kph`;
+  return (
+    <>
+      <h3>Region: {region}</h3>
+      <h4>Summary: {text}</h4>
+      <ul>
+        <li>Temperature is {temp_c} Celsius</li>
+        <li>Precipitation is {precip_mm}mm</li>
+        <li>Cloud level is at {cloud}</li>
+        <li>Wind is at {wind_kph}kph</li>
+      </ul>
+    </>
+  );
 }
