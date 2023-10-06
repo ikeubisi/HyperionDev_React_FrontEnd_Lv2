0eff17edb7a3db2e6a6300ba1f0c545204c60bf0
mark <markpackham1@gmail.com>
2023-10-06 09:16:35 +0100

    fetch the first country id

--- L2T06 - React - Hooks/Compulsory Task 1/nationality_name/src/components/NationalityName.jsx
+++ L2T06 - React - Hooks/Compulsory Task 1/nationality_name/src/components/NationalityName.jsx
@@ -4,10 +4,25 @@ import { useState } from "react";
 
 export default function NationalityName() {
   const [username, setUsername] = useState("");
+  const [nationality, setNationality] = useState("");
   const inputRef = useRef();
+  const siteUrl = "https://api.nationalize.io?name=michael";
 
+  // Input focus on mount
   useEffect(() => {
     inputRef.current.focus();
+  }, []);
+
+  // Fetch API
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(siteUrl);
+      let data = await response.json();
+      let firstCountryId = data.country[0].country_id;
+      console.log(firstCountryId);
+      setNationality(firstCountryId);
+    }
+    fetchData();
   }, []);
 
   return (
@@ -17,6 +32,7 @@ export default function NationalityName() {
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+      <h1>First Country ID: {nationality}</h1>
     </div>
   );
 }
