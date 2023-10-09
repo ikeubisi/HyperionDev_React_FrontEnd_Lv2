1c9b30d6964b4e9b6d274c6a6cc1b0ded9692d43
mark <markpackham1@gmail.com>
2023-10-09 10:41:16 +0100

    use my default stylings

--- L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
+++ L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
@@ -1,4 +1,5 @@
 import { Link, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/Home";
 import Products from "./components/Products";
@@ -6,18 +7,36 @@ import About from "./components/About";
 
 function App() {
   return (
-    <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/about">About</Link>
+    <div className="container">
+      {/* If this was larger than a 3 page site I'd use a separate component and call it Navbar */}
+      <nav className="navbar navbar-expand-lg mb-3 rounded">
+        <div className="container">
+          <ul className="navbar-nav">
+            <li className=" nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className=" nav-item">
+              <Link className="nav-link" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className=" nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
+
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
