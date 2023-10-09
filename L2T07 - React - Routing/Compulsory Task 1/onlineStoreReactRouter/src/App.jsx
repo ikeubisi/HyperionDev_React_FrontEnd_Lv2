f4f158d20f73555daa6a646a0484b03a69e20927
mark <markpackham1@gmail.com>
2023-10-09 10:21:52 +0100

    setup react router and navbar

---
+++ L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Routes>
+      <Navbar />
+    </Routes>
+  );
+}
+
+export default App;
