ead4873c77cceb50ee8b36ce7f05131e4f812d73
mark <markpackham1@gmail.com>
2023-10-09 13:07:34 +0100

    welcome user that logs in

--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/Home.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/Home.jsx
@@ -1,16 +1,48 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 export default function Home() {
-  const loginName = useRef();
+  // use state to hold username, welcome name and login label
+  const [username, setUsername] = useState("Admin");
+  const [welcomeName, setWelcomeName] = useState("Admin");
+  const [loginLabel, setLoginLabel] = useState("Login");
+
+  const handleWelcome = () => {
+    // user cannot login if field is empty
+    if (username.length > 0) {
+      setWelcomeName(username);
+      setLoginLabel("Logout");
+    } else {
+      alert("Please enter a username");
+    }
+  };
+
+  // clear the welcome on logout
+  const handleLogout = () => {
+    setWelcomeName("");
+    setLoginLabel("Login");
+  };
 
   return (
     <>
       <h1>Home</h1>
-      <input ref={loginName} defaultValue="" id="loginName" />
 
-      <button className="btn btn-primary" onClick={() => {}}>
-        Login
+      <h1>Welcome {welcomeName}</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        id="loginName"
+      />
+
+      <button className="btn btn-primary" onClick={handleWelcome}>
+        {loginLabel}
       </button>
+      {/* Hide logout button until user has entered their name */}
+      {loginLabel === "Logout" && (
+        <button className="btn btn-secondary" onClick={handleLogout}>
+          Logout
+        </button>
+      )}
     </>
   );
 }
