a0a5129a285889be5ba1749fa85b6078e4054c38
mark <markpackham1@gmail.com>
2023-10-09 10:25:52 +0100

    setup routes

--- L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
+++ L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
@@ -1,11 +1,15 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import About from "./components/About";
 
 function App() {
   return (
     <Routes>
-      <Navbar />
+      <Route exact path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products" element={<About />} />
     </Routes>
   );
 }
