4135259b67025f05552bff66e47085a6cffca877
mark <markpackham1@gmail.com>
2023-10-09 13:39:42 +0100

    tidy up the About section

--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/About.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/About.jsx
@@ -1,3 +1,29 @@
 export default function About() {
-  return <h3>About</h3>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Welcome to our store where you will find the highest quality products
+        for the lowest prices.
+      </p>
+      <h3>Please contact us at:</h3>
+      <div className="card mb-1">
+        <div className="card-body">
+          <ul>
+            <li className="mb-2">
+              <a href="mailto:reactRouterStore@gmail.com">
+                <i className="fa-solid fa-envelope"></i>&nbsp;
+                reactRouterStore@gmail.com
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="tel:+440022334455">
+                <i className="fa-solid fa-phone"></i>&nbsp; +440022334455
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
