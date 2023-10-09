bae058476dabb5cc97e6288c9f62f0494612014b
mark <markpackham1@gmail.com>
2023-10-09 15:21:16 +0100

    cite card usage from

--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/Products.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/Products.jsx
@@ -1,3 +1,14 @@
+// Learning about React Bootstrap Card from
+// Cards (no date) React Bootstrap.
+// Available at: https://react-bootstrap.github.io/docs/components/cards/ (Accessed: 09 October 2023).
+import Card from "react-bootstrap/Card";
 export default function Products() {
-  return <h2>Products</h2>;
+  return (
+    <>
+      <h2>Products</h2>
+      <Card>
+        <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+    </>
+  );
 }
