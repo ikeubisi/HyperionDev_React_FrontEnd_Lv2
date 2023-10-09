360ddd9418aaa48194f7a5639a45b316386afc2b
mark <markpackham1@gmail.com>
2023-10-09 11:23:01 +0100

    add button

--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/Home.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/Home.jsx
@@ -1,3 +1,16 @@
+import { useRef } from "react";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const loginName = useRef();
+
+  return (
+    <>
+      <h1>Home</h1>
+      <input ref={loginName} defaultValue="" id="loginName" />
+
+      <button className="btn btn-primary" onClick={() => {}}>
+        Login
+      </button>
+    </>
+  );
 }
