44dee7d890269e8a18cc5ee3cfa06c72b32f8469
mark <markpackham1@gmail.com>
2023-10-09 10:29:54 +0100

    add menu

--- L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
+++ L2T07 - React - Routing/Compulsory Task 1/onlineStoreReactRouter/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Products from "./components/Products";
@@ -6,11 +6,18 @@ import About from "./components/About";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/products" element={<About />} />
-    </Routes>
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
