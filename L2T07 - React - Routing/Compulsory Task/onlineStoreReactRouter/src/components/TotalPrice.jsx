dc555b32416884540c825a4cd56948b4eed784bf
mark <markpackham1@gmail.com>
2023-10-10 13:26:49 +0100

    have clear button for localStorage

--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/TotalPrice.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/components/TotalPrice.jsx
@@ -1,8 +1,27 @@
 export default function TotalPrice() {
   const localPrice = localStorage.getItem("localPrice");
 
+  // Clear local storage (alternate solution would be to set localPrice to zero but it's the only thing we're storing)
+  function clearPrice() {
+    localStorage.clear();
+    // Refresh the page
+    location.reload();
+  }
   // Hide total price if nothing has been bought
   return (
-    <>{localPrice > 0 && <h2 id="totalPrice">Total Price £{localPrice}</h2>}</>
+    <div>
+      {localPrice > 0 && (
+        <div id="priceSection">
+          <h2 id="totalPrice">Total Price £{localPrice}</h2>{" "}
+          <button
+            id="clearBtn"
+            className="btn btn-danger m-3"
+            onClick={clearPrice}
+          >
+            Clear Price
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/main.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/main.jsx
@@ -4,15 +4,11 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
