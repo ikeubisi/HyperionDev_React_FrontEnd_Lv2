9b2210d925f92098c43b5731259735739a485923
mark <markpackham1@gmail.com>
2023-10-11 07:42:31 +0100

    add formik styling and code

--- L2T08 - React - Form Validation/Tasks/formikDemo/src/main.jsx
+++ L2T08 - React - Form Validation/Tasks/formikDemo/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: { email: "" },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
