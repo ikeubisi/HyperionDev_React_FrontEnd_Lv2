00aa09ccf9ce32335059a340d60ad8ee40c76142
mark <markpackham1@gmail.com>
2023-10-11 08:01:27 +0100

    put Navbar in it's own component

--- L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/App.jsx
+++ L2T07 - React - Routing/Compulsory Task/onlineStoreReactRouter/src/App.jsx
@@ -1,35 +1,14 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About";
 import Home from "./components/Home";
 import Products from "./components/Products";
+import Navbar from "./components/sitewide/Navbar";
 
 function App() {
   return (
     <div className="container">
-      {/* If this was larger than a 3 page site I'd use a separate component and call it Navbar */}
-      <nav className="navbar navbar-expand-lg mb-3 rounded">
-        <div className="container">
-          <ul className="navbar-nav">
-            <li className=" nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className=" nav-item">
-              <Link className="nav-link" to="/products">
-                Products
-              </Link>
-            </li>
-            <li className=" nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
