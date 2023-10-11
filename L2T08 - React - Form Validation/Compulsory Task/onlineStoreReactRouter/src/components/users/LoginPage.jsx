30bc5ae485c5c1a4ecec52764ae4fa3fb19d148b
mark <markpackham1@gmail.com>
2023-10-11 09:46:27 +0100

    add login page data

--- L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/components/users/LoginPage.jsx
+++ L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/components/users/LoginPage.jsx
@@ -1,3 +1,41 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <>
+      <h1>Login</h1>
+      <hr />
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          className="m-2"
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
--- L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/components/users/RegisterPage.jsx
+++ L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/components/users/RegisterPage.jsx
@@ -22,52 +22,56 @@ export default function RegisterPage() {
     },
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-        className="m-2"
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
-      ) : null}
+    <>
+      <h1>Register</h1>
+      <hr />
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
+          className="m-2"
+        />
+        {formik.touched.firstName && formik.errors.firstName ? (
+          <div>{formik.errors.firstName}</div>
+        ) : null}
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-        className="m-2"
-      />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
-      ) : null}
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+          className="m-2"
+        />
+        {formik.touched.lastName && formik.errors.lastName ? (
+          <div>{formik.errors.lastName}</div>
+        ) : null}
 
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-        className="m-2"
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          className="m-2"
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
 
-      <button className="btn btn-primary" type="submit">
-        Submit
-      </button>
-    </form>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
