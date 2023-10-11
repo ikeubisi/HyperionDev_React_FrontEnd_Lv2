f044a3740af0daf4528b78c91452afa2f1ea004a
mark <markpackham1@gmail.com>
2023-10-11 08:42:43 +0100

    remove obsolete root element

--- L2T08 - React - Form Validation/Tasks/formikDemo/src/main.jsx
+++ L2T08 - React - Form Validation/Tasks/formikDemo/src/main.jsx
@@ -102,8 +102,4 @@ function App() {
   return <SignupForm />;
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />,
-  rootElement
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
