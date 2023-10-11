fbe032b97e3c35dae2319feff23bcddb957130f5
mark <markpackham1@gmail.com>
2023-10-11 09:27:10 +0100

    add login and register pages

--- L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/App.jsx
+++ L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/App.jsx
@@ -2,8 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About";
 import Home from "./components/Home";
-import Products from "./components/Products";
+import LoginPage from "./components/users/LoginPage";
 import Navbar from "./components/sitewide/Navbar";
+import Products from "./components/Products";
+import RegisterPage from "./components/users/RegisterPage";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
--- L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/components/sitewide/Navbar.jsx
+++ L2T08 - React - Form Validation/Compulsory Task/onlineStoreReactRouter/src/components/sitewide/Navbar.jsx
@@ -20,6 +20,16 @@ export default function Navbar() {
               About
             </Link>
           </li>
+          <li className=" nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className=" nav-item">
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
