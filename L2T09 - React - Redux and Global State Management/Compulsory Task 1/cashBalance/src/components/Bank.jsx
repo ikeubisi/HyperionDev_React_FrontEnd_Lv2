b971fffe1ec4b572f79c6ccd27990aa8c935a9f6
mark <markpackham1@gmail.com>
2023-10-12 18:13:26 +0100

    add input button

--- L2T09 - React - Redux and Global State Management/Compulsory Task 1/cashBalance/src/components/Bank.jsx
+++ L2T09 - React - Redux and Global State Management/Compulsory Task 1/cashBalance/src/components/Bank.jsx
@@ -10,30 +10,39 @@ export default function Bank() {
     <div>
       <h1>Bank Display</h1>
       <h3>Bank Balance: £{state.balance}</h3>
-      <ButtonReusable
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className={"btn btn-success"}
-      >
-        Deposit (+)
-      </ButtonReusable>
-      <ButtonReusable
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className={"btn btn-danger"}
-      >
-        Widthdraw (-)
-      </ButtonReusable>
-      <ButtonReusable
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className={"btn btn-warning"}
-      >
-        Bank Chargers (-)
-      </ButtonReusable>
-      <ButtonReusable
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className={"btn btn-info"}
-      >
-        Add Interest (*)
-      </ButtonReusable>
+      <div className="form-group">
+        <input
+          type="number"
+          placeholder="Enter Amount"
+          className="form-control m-2"
+        />
+      </div>
+      <div className="form-group">
+        <ButtonReusable
+          onClick={() => dispatch({ type: "INCREMENT" })}
+          className={"btn btn-success"}
+        >
+          Deposit (+)
+        </ButtonReusable>
+        <ButtonReusable
+          onClick={() => dispatch({ type: "INCREMENT" })}
+          className={"btn btn-danger"}
+        >
+          Widthdraw (-)
+        </ButtonReusable>
+        <ButtonReusable
+          onClick={() => dispatch({ type: "INCREMENT" })}
+          className={"btn btn-warning"}
+        >
+          Bank Chargers (-)
+        </ButtonReusable>
+        <ButtonReusable
+          onClick={() => dispatch({ type: "INCREMENT" })}
+          className={"btn btn-info"}
+        >
+          Add Interest (*)
+        </ButtonReusable>
+      </div>
     </div>
   );
 }
