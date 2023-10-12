99f7edbffd0caa8ff6ec41130c705905e698811b
mark <markpackham1@gmail.com>
2023-10-12 16:50:14 +0100

    demo reducer in action

--- L2T09 - React - Redux and Global State Management/Tasks/reducerDemo/src/GlobalState.js
+++ L2T09 - React - Redux and Global State Management/Tasks/reducerDemo/src/GlobalState.js
@@ -1,10 +1,6 @@
 export const initialState = {
   count: 0,
 };
-// If the state parameter is not provided (i.e., it is undefined),
-// it will default to the initialState object
-// This means that when the reducer is initially called,
-// it will use the initial state as the starting state.
 export const reducers = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return { ...state, count: state.count + 1 };
