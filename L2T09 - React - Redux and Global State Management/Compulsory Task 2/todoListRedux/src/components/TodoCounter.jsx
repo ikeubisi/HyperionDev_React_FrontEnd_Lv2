9c5f3a2e2dfcacc1d59d3b13b36012b2dc760d58
mark <markpackham1@gmail.com>
2023-10-13 10:45:29 +0100

    update todos count

--- L2T09 - React - Redux and Global State Management/Compulsory Task 2/todoListRedux/src/components/TodoCounter.jsx
+++ L2T09 - React - Redux and Global State Management/Compulsory Task 2/todoListRedux/src/components/TodoCounter.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
+
 const TodoCounter = () => {
+  // Grab todos from store
+  let todosCount = useSelector((state) => state.todos.length);
+
   return (
     // Keep at top of screen even when scrolling
     <div className="sticky-top m-2">
-      <h3>Todo List Counter:</h3>
+      <h3>Todo List Counter: {todosCount}</h3>
     </div>
   );
 };
