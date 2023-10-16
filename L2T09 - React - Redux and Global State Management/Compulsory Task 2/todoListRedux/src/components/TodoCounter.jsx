24a65863708ceabea69e331e0a399d05ea148838
mark <markpackham1@gmail.com>
2023-10-16 14:17:09 +0100

    style todo counter

--- L2T09 - React - Redux and Global State Management/Compulsory Task 2/todoListRedux/src/components/TodoCounter.jsx
+++ L2T09 - React - Redux and Global State Management/Compulsory Task 2/todoListRedux/src/components/TodoCounter.jsx
@@ -12,11 +12,15 @@ const TodoCounter = () => {
   return (
     // Keep at top of screen even when scrolling
     <div className="sticky-top m-2">
-      <h3>Total Todos: {todosCount.length}</h3>
-      <h3 className="text-success">Completed Todos: {completedTodos.length}</h3>
-      <h3 className="text-danger">
-        Incomplete Todos: {todosCount.length - completedTodos.length}
-      </h3>
+      <div className="todoCounter">
+        <h3>Total Todos: {todosCount.length}</h3>
+        <h3 className="text-success">
+          Completed Todos: {completedTodos.length}
+        </h3>
+        <h3 className="text-danger">
+          Incomplete Todos: {todosCount.length - completedTodos.length}
+        </h3>
+      </div>
     </div>
   );
 };
