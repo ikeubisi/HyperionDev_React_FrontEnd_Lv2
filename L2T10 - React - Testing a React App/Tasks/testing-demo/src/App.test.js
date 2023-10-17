59b3b6908037dddd991103c183567b53d7483133
mark <markpackham1@gmail.com>
2023-10-17 10:16:23 +0100

    decrement test by 1 result

--- L2T10 - React - Testing a React App/Tasks/testing-demo/src/App.test.js
+++ L2T10 - React - Testing a React App/Tasks/testing-demo/src/App.test.js
@@ -2,12 +2,6 @@ import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 import renderer from "react-test-renderer";
 
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test("App renders correctly", () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
@@ -18,4 +12,12 @@ test("increments the count", () => {
   const incrementButton = getByText("Increment");
   fireEvent.click(incrementButton);
   expect(getByText("Count: 1")).toBeInTheDocument();
+});
+
+// Once it has been increased by 1 decrement to 0
+test("decrements the count", () => {
+  const { getByText } = render(<App />);
+  const decrementButton = getByText("Decrement");
+  fireEvent.click(decrementButton);
+  expect(getByText("Count: -1")).toBeInTheDocument();
 });
