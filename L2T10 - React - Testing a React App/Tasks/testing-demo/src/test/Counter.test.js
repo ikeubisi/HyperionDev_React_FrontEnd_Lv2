bfd9ed61152bcf6b2616655c8ab03238063417b7
mark <markpackham1@gmail.com>
2023-10-17 09:35:04 +0100

    add decrement test

--- L2T10 - React - Testing a React App/Tasks/testing-demo/src/test/Counter.test.js
+++ L2T10 - React - Testing a React App/Tasks/testing-demo/src/test/Counter.test.js
@@ -1,8 +1,18 @@
 import { render, fireEvent } from "@testing-library/react";
 import App from "../App";
+
 test("increments the count", () => {
   const { getByText } = render(<App />);
   const incrementButton = getByText("Increment");
   fireEvent.click(incrementButton);
   expect(getByText("Count: 1")).toBeInTheDocument();
+});
+
+// After count increased to 1 it should then get decremented
+
+test("decrement the count", () => {
+  const { getByText } = render(<App />);
+  const decrementButton = getByText("Decrement");
+  fireEvent.click(decrementButton);
+  expect(getByText("Count: 0")).toBeInTheDocument();
 });
