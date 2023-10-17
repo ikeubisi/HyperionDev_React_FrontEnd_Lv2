a69b2d66aaa326827e8f545462b7a85a64f2c99e
mark <markpackham1@gmail.com>
2023-10-17 09:01:18 +0100

    create react test demo app

---
+++ L2T10 - React - Testing a React App/Tasks/testing-demo/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const buttonStyle = {
+    backgroundColor: "lightblue",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    fontSize: "1rem",
+  };
+
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Simple Counter App</h1>
+      <p>Count: {count}</p>
+      <button
+        style={{ ...buttonStyle, marginRight: "10px" }}
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+      <button style={buttonStyle} onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default App;
